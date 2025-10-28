0af996613eb8a36c6184ac6b5c3b8767ca70a4c1
IGrock88 <igrock88@gmail.com>
2025-10-28 11:42:30 +0800

    Update ParallaxImage.js

--- src/parallaximage/ParallaxImage.js
+++ src/parallaximage/ParallaxImage.js
@@ -17,7 +17,7 @@ export default class ParallaxImage extends Component {
         sliderHeight: PropTypes.number, // passed from <Carousel />
         sliderWidth: PropTypes.number, // passed from <Carousel />
         vertical: PropTypes.bool, // passed from <Carousel />
-        containerStyle: ViewPropTypes ? ViewPropTypes.style : View.propTypes.style,
+        containerStyle: ViewPropTypes.style,
         dimensions: PropTypes.shape({
             width: PropTypes.number,
             height: PropTypes.number
